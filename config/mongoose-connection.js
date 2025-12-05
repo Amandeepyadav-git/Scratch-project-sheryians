1c5a50df2bdf12a0f0fa80bc66481df4ab310b9d
Amandeep Yadav <amandeepyadav0704@gmail.com>
2025-12-05 17:16:15 +0530

    New update to initial commit

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
+const dbgr = require('debug')("cdevelopment:mongoose");
+const config = require('config')
 
 mongoose
-.connect("mongodb://127.0.0.1:27017/scratch")
-.then(function(){
-    console.log("connected");
+    .connect(`${config.get("MONGODB_URI")}/scratch`)
+    .then(function(){
+        dbgr("connected");
     
-})
-.catch(function(err){
-    console.log(err);
-})
+    })
+    .catch(function(err){
+    dbgr(err);
+    })
 
 module.exports = mongoose.connection;
