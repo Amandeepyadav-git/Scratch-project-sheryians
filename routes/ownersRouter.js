26cc5e68a9cc65533a5b3323665ccfefd5ed1e18
Amandeep Yadav <amandeepyadav0704@gmail.com>
2025-12-10 19:01:56 +0530

    Update ownersRouter.js

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res)=>{
-    res.send("Hey, it's working");
+    res.send("Hey I'm owner, it's working");
 })
 
 module.exports = router;
