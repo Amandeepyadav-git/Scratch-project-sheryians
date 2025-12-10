2877d906c2057a379dbb152b41f8f995cce41d5a
Amandeep Yadav <amandeepyadav0704@gmail.com>
2025-12-10 19:02:12 +0530

    Update productsRouter.js

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res)=>{
-    res.send("Hey, it's working");
+    res.send("Hey I'm product, it's working");
 })
 
 module.exports = router;
